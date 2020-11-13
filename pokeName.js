6d3670014feb123a79175556911abb950015e40e
terranadams <terran.adams7225@stu.mtec.edu>
2020-11-12 22:50:31 -0700

    display all types of each pokemon instead of just one

--- pokeName.js
+++ pokeName.js
@@ -9,13 +9,18 @@ export async function getDataByName(pokeName) {
                     `;
 
     let name = res.name
-    let type = res.types[0].type.name
     document.getElementById('name').innerHTML = `Name: ${name.charAt(0).toUpperCase() + name.slice(1)}`
-    document.getElementById('type').innerHTML = `Type: ${type.charAt(0).toUpperCase() + type.slice(1)}`
+
+    let types = res.types.map(x => x.type.name)
+    let typesHTML = `Type(s): `
+    types.forEach(x => {
+        typesHTML += `${x.charAt(0).toUpperCase() + x.slice(1)}, `
+    })
+    document.getElementById('type').innerHTML = typesHTML
 
     let abilities = res.abilities.map(abi => abi.ability.name)
     let abilitiesHTML = `Abilities: `
-    abilities.map(a => abilitiesHTML += ` ${a.charAt(0).toUpperCase() + a.slice(1)}`)
+    abilities.map(a => abilitiesHTML += ` ${a.charAt(0).toUpperCase() + a.slice(1)}, `)
     document.getElementById('abilities').innerHTML = abilitiesHTML
 
 }
--- pokeNumber.js
+++ pokeNumber.js
@@ -1,22 +1,26 @@
 export async function getDataByNumber() {
 
-    const pokemonData = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 932) + 1}/`
-        )
+    const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 932) + 1}/`)
     let res = await pokemonData.json()
     console.log(res)
     let thePhoto = res.sprites.front_default
     document.getElementById('photoArea').innerHTML = `
                     <img src="${thePhoto}" width="300px" height="300px">
                     `;
+
     let name = res.name
-    let type = res.types[0].type.name
     document.getElementById('name').innerHTML = `Name: ${name.charAt(0).toUpperCase() + name.slice(1)}`
-    document.getElementById('type').innerHTML = `Type: ${type.charAt(0).toUpperCase() + type.slice(1)}`
+
+    let types = res.types.map(x => x.type.name)
+    let typesHTML = `Type(s): `
+    types.forEach(x => {
+        typesHTML += `${x.charAt(0).toUpperCase() + x.slice(1)}, `
+    })
+    document.getElementById('type').innerHTML = typesHTML
 
     let abilities = res.abilities.map(abi => abi.ability.name)
     let abilitiesHTML = `Abilities: `
-    abilities.map(a => abilitiesHTML += ` ${a.charAt(0).toUpperCase() + a.slice(1)}`)
+    abilities.map(a => abilitiesHTML += ` ${a.charAt(0).toUpperCase() + a.slice(1)}, `)
     document.getElementById('abilities').innerHTML = abilitiesHTML
 
 }
